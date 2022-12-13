a7f22f2d4823553b4e12ee568e8059477e9c8b25
akinoriakatsuka <akinoriakatsuka@gmail.com>
2022-12-13 20:18:22 +0900

    Initial Commit

---
+++ LiveActivities/LiveActivitiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveActivitiesApp.swift
+//  LiveActivities
+//
+//  Created by 赤塚啓紀 on 2022/12/13.
+//
+
+import SwiftUI
+
+@main
+struct LiveActivitiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
